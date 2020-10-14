db8075991404265b09d4ecb48d83dc4909beef12
980765465@qq.com <lkd980765465>
2020-10-14 16:52:57 +0800

    开始学习 python

--- nodeProject/webServer/route/web.js
+++ nodeProject/webServer/route/web.js
@@ -3,10 +3,8 @@ const express = require('express');
 module.exports = function () {
     const router = express.Router();
     router.get('/',function (req,res) {
-        //console.log(req)
-        console.log('fdsafsadfsdfsa');
+        console.log(req.query);
         console.log('get')
-
         let data = {
             success:'Y',
             message:'data save success'
@@ -16,14 +14,26 @@ module.exports = function () {
     })
     router.post('/',function (req,res) {
         console.log('post')
+        console.log(req.body)
         let data = {
             success:'Y',
             message:'data save success'
         }
-        console.log(req.host)
-        console.log(req.body)
-        // console.log(req)
-        res.send(data).end()
+        let data2 = {
+          data: {
+            orderId: "orderId",
+            policyNo: "policyNo",
+            policyUrl: "https://www.baidu.com/",
+            productName: "华泰医疗保险",
+            proposalNo: "proposalNo",
+            totalPremium: "totalPremium"
+          },
+          reasonCode: "10200",
+          reasonDesc: "成功",
+          success: true,
+          totalCount: null
+        }
+        res.send(data2).end()
     })
     return router
 }
--- nodeProject/webServer/server.js
+++ nodeProject/webServer/server.js
@@ -17,12 +17,14 @@ server.listen(3389);
 // server.all('*', function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Headers", "content-type");
 //     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//     res.header("X-Powered-By",' 3.2.1')
+//     res.header("X-Powered-By",'3.2.1')
 //     res.header("Content-Type", "application/json;charset=utf-8");
 //     next();
 // });
 // 获取请求
+server.use(bodyParser.json())
 server.use(bodyParser.urlencoded())
 server.use(multerObj.any())
 
