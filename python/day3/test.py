1614058590f9ae60f557385bd830058345cd5804
980765465@qq.com <lkd980765465>
2020-10-19 21:30:48 +0800

    python 练习

---
+++ python/day3/test.py
@@ -0,0 +1,34 @@
+import xlwings as xw
+# wb = xw.Book('./test.xlsx')
+#
+# sht = wb.sheets['Sheet1']
+
+# print(wb.fullname)
+# print(sht.name)
+# shtdata = sht.range('a1').expand().value
+# sht.range('A1').value = 'xlwings'
+# print(sht.range('A1').row)
+# print(sht.range('A1').column)
+# print(sht.range('A1').row_height)
+# print(sht.range('A1').column_width)
+# result = []
+# for item in shtdata:
+# 	title = shtdata[0]
+# 	for titleItem in title:
+# 		itemdata = {
+# 			title[0]: item[0],
+# 			title[1]: item[1],
+# 			title[2]: item[2],
+# 			title[3]: item[3]
+# 		}
+# 		result.append(itemdata)
+#
+# print(result)
+# fo = open('foo.txt','w')
+# fo.write(str(result))
+# fo.close()
+
+test1 = 1
+test1 += 1
+print(test1)
+
