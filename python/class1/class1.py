5c15d573e500dfc8e5179950d9259f7ae342e80d
980765465@qq.com <lkd980765465>
2020-10-21 09:26:33 +0800

    python 练习

--- python/class1/class1.py
+++ python/class1/class1.py
@@ -1,6 +1,7 @@
 # _*_ coding: utf-8 _*_
 import os
-
+import glob
+import re
 print (os.getcwd())
 print (213)
 path = os.path.join('myproject','AI')
@@ -13,7 +14,16 @@ print (path)
 
 files1 = os.scandir()
 for item in files1:
-    print (item.name,item.path,item.isdir)
+    print(item.name,item.path,item.is_dir(),item.is_file())
+
+for root, dirs, files in os.walk('.', topdown=True):
+	for name in files:
+		print(os.path.join(root,name))
+	for name in dirs:
+		print(name)
+
+print(glob.glob('*.py'))
+print(re.findall(r'\bf[a-z]*','whiich foot or hand fell fastest'))
 '''
     os 文件模块
     getcwd() 获取当前路径
