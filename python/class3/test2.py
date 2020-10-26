9bff808889400e36722485f38a18a9015b0d98a7
980765465@qq.com <lkd980765465>
2020-10-26 21:19:41 +0800

    python 练习

--- python/class3/test2.py
+++ python/class3/test2.py
@@ -10,44 +10,58 @@ import random
 # print(random.choice(range(0,10)))
 # print(random.choice([3,4,5,6,7,9,0]))
 # print(random.random())
-print(random.randrange(0,10,2))
-a = [1,2,3,4,5,6]
-random.shuffle(a)
-print(a)
-
-list1 = [1,2,3]
-list2 = [4,5,6]
-list1.extend(list2)
-print(list1)
-a.sort()
-print(a)
-
-b = {'a':1,'b':2,'c':{'d':'fdsafdsa','e':{'f':'1232'}}}
-print(b)
+# print(random.randrange(0,10,2))
+# a = [1,2,3,4,5,6]
+# random.shuffle(a)
+# print(a)
+#
+# list1 = [1,2,3]
+# list2 = [4,5,6]
+# list1.extend(list2)
+# print(list1)
+# a.sort()
+# print(a)
+#
+# b = {'a':1,'b':2,'c':{'d':'fdsafdsa','e':{'f':'1232'}}}
+# print(b)
 # b.clear()
 # print(b)
-c = b.copy()
-print(c)
+# c = b.copy()
+# print(c)
+# testset = set(('baidu','alibaba','tengxun','wangyi'))
+# print(testset)
+# testset.add('pingduoduo')
+# print(testset)
+# testset.clear()
+# print(testset)
+# testset2 = testset.copy()
+# print(testset2)
+# testset3 = set(('baidu','alibaba'))
+# print(testset2.difference(testset3))
+# testset2.difference_update(testset3)
+# print(testset2)
+# testset.discard('baidu')
+# print(testset)
+# print(testset.intersection(testset3))
+#
+# list = [1,2,3,4]
+# it = iter(list)
+# for x in it:
+# 	print(x, end=" ")
 
+stack = [3,4,5]
+stack.append(6)
+stack.append(7)
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+stack.pop()
+stack.pop()
+print(stack)
 
+dict = {'name':'frank','sex':'男','age':28,'say':'hello mother fucker'}
+for key,val in dict.items():
+	print(key,val)
 
-
-
-len(dict)
-计算字典元素个数，即键的总数。
->>> dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
->>> len(dict)
-3
-2	str(dict)
-输出字典，以可打印的字符串表示。
->>> dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
->>> str(dict)
-"{'Name': 'Runoob', 'Class': 'First', 'Age': 7}"
-3	type(variable)
-返回输入的变量类型，如果变量是字典就返回字典类型。
->>> dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
->>> type(dict)
-<class 'dict'>
-Python字典包含了以下内置方法：
-
-序号	函数及描述
+while
